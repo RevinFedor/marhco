25048fcb7821dcc6930e4806e191fd4cc010acad
RevinFedor <fr75793@gmail.com>
2022-04-24 15:29:00 +0500

    added promo and  partners

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,48 @@ $(function(){
       readOnly: true,
   });
 
+  
+  function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector('.fashion__clock');
+    const daysSpan = clock.querySelector('.fashion__days');
+    const hoursSpan = clock.querySelector('.fashion__hours');
+    const minutesSpan = clock.querySelector('.fashion__minutes');
+    const secondsSpan = clock.querySelector('.fashion__seconds');
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.fashion__clock').attr('data-time');
+  initializeClock('.fashion__clock', deadline);
+
 })
